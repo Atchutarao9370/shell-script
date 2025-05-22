de87304781147ca9398063cbfe0de0f2d94b3315
Atchutarao <atchutaraochappa9370@gmail.com>
2025-05-22 16:23:46 +0530

    sheel script

---
+++ 07-install-script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+abcdefg=(id= -u)
+
+if [ $abcdefg -ne 0 ]
+then 
+    echo "Error:: You must have access to execute the script"
+    exit 1 #other tahn 0
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MYSQL ... FAILURE"
+        exit 1
+    else
+        echo "Installing MYSQL ... SUCCESS"
+    fi
+else
+    echo "MYSQL is already ... INSTALLED"
+fi
