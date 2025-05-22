5ba4823c0161a47a62cd2b145a9d14443077791a
Atchutarao <atchutaraochappa9370@gmail.com>
2025-05-22 14:58:27 +0530

    sheel script

---
+++ 04-arrar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+movies=("pushpa" "rrr" "devara")
+
+echo "first movie: ${movies[0]}"
+echo "first movie: ${movies[1]}"
+echo "first movie: ${movies[2]}"
+echo "first movie: ${movies[@]}"
